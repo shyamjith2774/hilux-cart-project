9b878c61a2391f1d4a00f66675738ccee05a7238
shyamjith27774 <shyamjithsasikumar2774@gmail.com>
2025-10-21 23:14:24 +0530

    cmplted orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div>Orders
-      
+    <div className='flex  items center justify-center text-2xl md:text-3xl  font-light mt-20'>
+     
+      <p>Your orders will be shown HERE...! </p>
     </div>
   )
 }
